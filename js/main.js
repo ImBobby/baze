4998fc8eb87504ea24c9be3b108b5e7d86d4e2fc
Andrew Manullang <andrewkmanullang@gmail.com>
2013-04-25 08:13:39 +0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+/*******************************************************************************
+
+    Title :  
+    Date  :  January 2013
+    Author:  Suitmedia (http://www.suitmedia.com)
+
+********************************************************************************/
+
+var Site = {
+
+    /**
+     * Init Function
+     */
+    init: function() {
+        $('html').removeClass('no-js');
+        Site.feature1();
+    },
+    
+    /**
+     * Site featured
+     */
+    feature1: function() {
+    }
+
+}
+
+
+$(function() {
+    Site.init();
+});
