46a80841628ac0303562c16ab48c45a767c34445
ImBobby <squidz.bee@gmail.com>
2013-11-13 10:47:57 +0700

    add fix windows phone device-width bug

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ var Site = {
     init: function() {
         Site.fastClick();
         Site.enableActiveStateMobile();
+        Site.windowsPhoneViewportFix();
     },
 
     fastClick: function() {
@@ -19,9 +20,24 @@ var Site = {
 
     enableActiveStateMobile: function(){
         document.addEventListener('touchstart', function(){}, true);
+    },
+
+    windowsPhoneViewportFix: function() {
+        /** 
+         * Fix windows phone viewport bug
+         * http://timkadlec.com/2013/01/windows-phone-8-and-device-width 
+         */
+         
+        if(navigator.userAgent.match(/IEMobile\/10\.0/)) {
+            var msViewportStyle = document.createElement("style"),
+                fix             = document.createTextNode("@-ms-viewport{width:auto!important}");
+
+            msViewportStyle.appendChild(fix);
+            document.getElementsByTagName('head')[0].appendChild(msViewportStyle);
+        }
     }
 
-}
+};
 
 
 $(function() {
